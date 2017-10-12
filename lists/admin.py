6079401f7891e7b057ec40a40f99b12e3aaa7ee7
qiaorun0707 <qiaorun0707@163.com>
2017-10-12 15:55:51 +0800

    部分3

--- lists/admin.py
+++ lists/admin.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from .models import Question
 from django.contrib import admin
 
+admin.site.register(Question)
 # Register your models here.
--- lists/views.py
+++ lists/views.py
@@ -4,8 +4,17 @@ from django.http import HttpResponse
 from django.template import loader
 from lists.models import Question
 
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
 
-def index(request):
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote (request, question_id):
+    return HttpResponse("You're voting on question %s.")
+
+def index(request, question_id):
     return HttpResponse("Hellp,world. You're at the polls index.")
 # Create your views here.
 
