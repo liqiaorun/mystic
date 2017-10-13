1a84f0990487c74229fb50cdb87b4977fbf114b7
qiaorun0707 <qiaorun0707@163.com>
2017-10-13 09:23:58 +0800

    shitu

--- lists/urls.py
+++ lists/urls.py
@@ -1,8 +1,15 @@
+# -*- coding:utf-8 -*-
 from django.conf.urls import url
 
 from . import views
 
 urlpatterns = [
+    # ex: /lists/
     url(r'^$', views.index, name='index'),
-    url(r'^temp$', views.index_temp, name='index_temp'),
+    # ex: /lists/5/
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    # ex: /lists/5/results/
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    # ex: /lists/5/vote
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote')
 ]
