814ae6b2f4b083422bbe69e0006b5cb4bc04d70d
qiaorun0707 <qiaorun0707@163.com>
2017-10-16 14:51:50 +0800

    sa qiaorun form ok 2017-10-16

--- lists/urls.py
+++ lists/urls.py
@@ -8,9 +8,11 @@ urlpatterns = [
     # ex: /lists/
     url(r'^$', views.index, name='index'),
     # ex: /lists/5/
-    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    # url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    # the 'name' value as called by the {% url %} template tag
+    url(r'^specifics/(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
     # ex: /lists/5/results/
     url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
     # ex: /lists/5/vote
-    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote')
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
--- lists/views.py
+++ lists/views.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.http import HttpResponse, Http404
+from django.http import HttpResponseRedirect, Http404, HttpResponse
 from django.template import loader
 from lists.models import Question
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 
+from .models import Choice, Question
 
 def detail(request, question_id):
     try:
@@ -15,17 +17,35 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+   question = get_object_or_404(Question, pk=question_id)
+   return render(request, 'lists/results.html', {'question': question})
+   # response = "You're looking at the results of question %s."
+   # return HttpResponse(response % question_id)
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s.")
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'lists/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('lists:results', args=(question.id)))
 
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
+    print(latest_question_list)
+    context = {'question': latest_question_list}
     return render(request, 'lists/index.html', context)
 # Create your views here.
 
